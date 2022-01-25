f2e9c180d9ab6f3f848bb28f32a8c6fc88ac8f74
office1201 <office.div1201@gmail.com>
2022-01-25 22:01:22 +0900

    add: 검색어 입력 시 x버튼 보임

--- js/views/FormView.js
+++ js/views/FormView.js
@@ -9,10 +9,20 @@ FormView.setup = function (el) {
   this.inputEl = document.querySelector('[type=text]');
   this.resetEl = document.querySelector('[type=reset]');
   this.showResetBtn(false);
+  this.bindEvents();
+  return this;
 };
 
 FormView.showResetBtn = function (show = true) {
   this.resetEl.style.display = show ? 'block' : 'none';
+};
+
+FormView.bindEvents = function () {
+  this.inputEl.addEventListener('keyup', () => this.onKeyup());
+};
+
+FormView.onKeyup = function () {
+  this.showResetBtn(this.inputEl.value.length);
 };
 
 export default FormView;
